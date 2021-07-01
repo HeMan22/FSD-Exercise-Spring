6a545ca555919df500df653966ed9b036686aba2
Himanshu Tripathi <heman.tri2208@gmail.com>
2021-07-02 02:34:37 +0530

    Added Docket configuration for API configuration with SWAGGER

--- Spring MVC/mycontactapi/src/main/java/com/practice/mycontactapi/config/WebApplicationConfig.java
+++ Spring MVC/mycontactapi/src/main/java/com/practice/mycontactapi/config/WebApplicationConfig.java
@@ -1,11 +1,18 @@
 package com.practice.mycontactapi.config;
 
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
+import springfox.documentation.builders.ApiInfoBuilder;
+import springfox.documentation.builders.PathSelectors;
+import springfox.documentation.builders.RequestHandlerSelectors;
+import springfox.documentation.service.ApiInfo;
+import springfox.documentation.spi.DocumentationType;
+import springfox.documentation.spring.web.plugins.Docket;
 import springfox.documentation.swagger2.annotations.EnableSwagger2;
 
 @Configuration
@@ -15,9 +22,27 @@ import springfox.documentation.swagger2.annotations.EnableSwagger2;
 public class WebApplicationConfig implements WebMvcConfigurer {
 	@Override
 	public void addResourceHandlers(ResourceHandlerRegistry registry) {
-		registry.addResourceHandler("swagger-ui.html").addResourceLocations("classpath:/META-INF/resources/");
+		registry.addResourceHandler("swagger-ui.html")
+		.addResourceLocations("classpath:/META-INF/resources/");
 
-		registry.addResourceHandler("/webjars/**").addResourceLocations("classpath:/META-INF/resources/webjars/");
+		registry.addResourceHandler("/webjars/**")
+		.addResourceLocations("classpath:/META-INF/resources/webjars/");
+	}
+	
+	@Bean
+	public Docket apiDocumentation() {
+		
+		return new Docket(DocumentationType.SWAGGER_2).select()
+				.paths(PathSelectors.regex("/api/v1/.*"))
+				.apis(RequestHandlerSelectors.basePackage("com.practice.mycontactapi.controller"))
+				.build()
+				.apiInfo(apiInfo());
+				
+	}
+
+	private ApiInfo apiInfo() {
+		
+		return new ApiInfoBuilder().description("My Contact API description").title("My Contact API").build();
 	}
 
 
